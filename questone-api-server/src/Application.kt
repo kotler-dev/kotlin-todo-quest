b5cb77cfaec20b1f0ff91163a2f4ff4a6eb30b7f
Tony Branch <tonybranch.org@yahoo.com>
2020-11-19 19:54:44 +0300

    Add method POST

--- questone-api-server/src/Application.kt
+++ questone-api-server/src/Application.kt
@@ -19,8 +19,8 @@ fun main() {
 
     val contacts = ArrayList<Contact>()
     fun initContacts() {
-        contacts.add(Contact("John", "123-45-67"))
-        contacts.add(Contact("Robert", "452-21-27"))
+//        contacts.add(Contact("John", "123-45-67"))
+//        contacts.add(Contact("Robert", "452-21-27"))
         contacts.add(Contact("Tony", "151-67-23"))
         contacts.add(Contact("Jully", "126-27-41"))
         contacts.add(Contact("Harper", "133-57-75"))
@@ -29,7 +29,7 @@ fun main() {
     initContacts()
 
     val app = Javalin.create().start(7777)
-    app.get("/") { ctx -> ctx.result("") }
+    app.get("/") { ctx -> ctx.result("Hello") }
 
     app.get("/contacts") { ctx -> ctx.json(contacts) }
 
@@ -44,6 +44,42 @@ fun main() {
             }
         } catch (ex: Exception) {
             ctx.result("Invalid input index.").status(404)
+        }
+    }
+
+    app.post("/contacts/:name/:phone") { ctx ->
+//        val name: String
+//        val phone: String
+
+        try {
+            val name = ctx.pathParam("name")
+            val phone = ctx.pathParam("phone")
+
+            if (name.trim().isNotEmpty() && phone.trim().isNotEmpty()) {
+
+                var unique: Boolean = true
+
+                for (item in contacts.indices) {
+                    val con = contacts[item]
+                    if (name.equals(con.name)) {
+                        unique = false
+                        break
+                    }
+                }
+
+                if (unique) {
+                    contacts.add(Contact(name, phone))
+                    ctx.result("Added data.").status(202)
+                } else {
+                    ctx.result("Please use unique name.").status(404)
+                }
+
+            } else {
+                ctx.result("You need to have a value for either the name or the phone.").status(404)
+            }
+
+        } catch (ex: Exception) {
+            ctx.result("Unknown error when creating phone entry.").status(404)
         }
     }
 
