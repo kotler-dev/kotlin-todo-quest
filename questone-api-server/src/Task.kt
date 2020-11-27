74a0ee153d0ab84a4fa033e345b87f4a17110841
Tony Branch <tonybranch.org@yahoo.com>
2020-11-27 09:58:24 +0300

    Update suspend main and class.

--- questone-api-server/src/Task.kt
+++ questone-api-server/src/Task.kt
@@ -1,8 +1,5 @@
 package dev.hyperhunt.kotlin
 
-import io.netty.handler.codec.http.HttpHeaders.getDate
-import java.time.Instant.now
-import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import java.util.*
@@ -15,8 +12,6 @@ import java.util.*
 //}
 
 class Task(_title: String, _isDone: Boolean) {
-
-    //    var id: String = _id
     val id: String = UUID.randomUUID().toString()
 
     var title = _title
@@ -25,16 +20,11 @@ class Task(_title: String, _isDone: Boolean) {
             field = value.trim()
         }
 
-//    fun generateUUID (): String {
-//        return UUID.randomUUID().toString()
-//    }
-
     val isDone = _isDone
 
     private var date = LocalDateTime.now()
 
     private val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS")
-    private val formatted = date.format(formatter)
-    val createdAt = formatted
+    val createdAt: String? = date.format(formatter)
 }
 
