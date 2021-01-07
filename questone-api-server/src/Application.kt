8f9e6e2bf1075c8fe5f030612406df97c1060325
Tony Branch <tonybranch.org@yahoo.com>
2021-01-07 19:57:03 +0300

    Patch: Mark task as done/undone.

--- questone-api-server/src/Application.kt
+++ questone-api-server/src/Application.kt
@@ -87,19 +87,7 @@ object TaskController {
                     it.isDone = !it.isDone // Mark task as done/undone
                 }
             }
-
             task.find { it.id == uuid }?.let { ctx.json(it) } ?: ctx.result("Patch: Not found task for change status.").status(400)
-//            val data = task.find { it.id == uuid } ?: NotFoundResponse()
-//            ctx.json(data).status(200)
-
-//            if (uuid.trim().isNotEmpty() && unique) {
-//
-////                ctx.result("Post: [Id unique]").status(200)
-//            } else {
-//                ctx.result("Post: Id not unique.").status(400)
-//            }
-
-
         } catch (ex: Exception) {
             ctx.result("Patch: ${ex.message}").status(400)
         }
@@ -145,129 +133,6 @@ fun main() {
     // /task/{uuid} Change task status: ['done', 'undone']
     // Mark task as done/undone
     app.patch("/tasks/:uuid", TaskController::patchChangeTaskStatus)
-
-/*
-
-
-    app.post("/todo/:name/:phone") { ctx ->
-//        val name: String
-//        val phone: String
-
-        try {
-            val name = ctx.pathParam("name")
-            val phone = ctx.pathParam("phone")
-
-            if (name.trim().isNotEmpty() && phone.trim().isNotEmpty()) {
-
-                var unique: Boolean = true
-
-                for (item in task.indices) {
-                    val con = task[item]
-                    if (name.equals(con.name)) {
-                        unique = false
-                        break
-                    }
-                }
-
-                if (unique) {
-                    task.add(DataTask(name, phone))
-                    ctx.result("Added data.").status(200)
-                } else {
-                    ctx.result("Please use unique name.").status(404)
-                }
-
-            } else {
-                ctx.result("You need to have a value for either the name or the phone.").status(404)
-            }
-
-        } catch (ex: Exception) {
-            ctx.result("Unknown error when creating phone entry.").status(404)
-        }
-    }
-
-    app.put("/todo/:id/:name/:phone") { ctx ->
-        try {
-            val idx = Integer.parseInt(ctx.pathParam("id"))
-            val name = ctx.pathParam("name")
-            val phone = ctx.pathParam("phone")
-
-            if (idx > -1 && idx < task.size) {
-                task.add(idx, DataTask(name, phone))
-                task.removeAt(idx + 1)
-                ctx.result("Put: Added new contact.")
-            } else {
-                ctx.result("Put: Invalid ID is out boundaries.").status(404)
-            }
-
-        } catch (ex: Exception) {
-            ctx.result("Put: Invalid input ID contact.").status(404)
-        }
-    }
-
-    app.patch("/todo/:name/:phone") { ctx ->
-        try {
-            val name = ctx.pathParam("name")
-            val phone = ctx.pathParam("phone")
-
-            if (name.trim().isNotEmpty()) {
-                for (item in task.indices) {
-//                    val con = todo[item]
-                    if (name == task[item].name) {
-                        task[item].phone = phone
-                        ctx.result("The phone number has been updated.").status(200)
-                    } else {
-                        ctx.result("Unknown error when updating phone entry.").status(404)
-                    }
-                }
-            } else {
-                ctx.result("Empty name.").status(404)
-            }
-        } catch (ex: Exception) {
-            ctx.result("Unknown error when replacing phone entry.").status(404)
-        }
-    }
-
-    app.patch("/todo/:id/:name/:phone") { ctx ->
-        try {
-            val idx = Integer.parseInt(ctx.pathParam("id"))
-            val name = ctx.pathParam("name")
-            val phone = ctx.pathParam("phone")
-            if (idx > -1 && idx < task.size) {
-                if (name.trim().isNotEmpty()) {
-                    task[idx].name = name
-                    ctx.result("Patch: Name has been changed.").status(200)
-                }
-                if (phone.trim().isNotEmpty()) {
-                    task[idx].phone = phone
-                    ctx.result("Patch: Phone number has been changed.").status(200)
-                }
-            } else {
-                ctx.result("Patch: ID is out boundaries.").status(404)
-            }
-        } catch (ex: Exception) {
-            ctx.result("Patch: Invalid input ID contact.").status(404)
-        }
-    }
-
-    app.delete("/todo/:id") { ctx ->
-        try {
-            val idx = Integer.parseInt(ctx.pathParam("id"))
-//            val name = ctx.pathParam("name")
-//            val phone = ctx.pathParam("phone")
-
-            if (idx > -1 && idx < task.size) {
-                task.removeAt(idx)
-                ctx.result("Delete: Deleted record with id (${idx}).").status(200)
-            } else {
-                ctx.result("Delete: Index is out boundaries.").status(404)
-            }
-
-        } catch (ex: Exception) {
-            ctx.result("Delete: Unknown error when deleting record.").status(404)
-        }
-    }
-
- */
 }
 
 
